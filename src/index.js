233666201d9339e135fb91a315197124897d0892
Tyler Hamlett <tylerbyte@users.noreply.github.com>
2017-05-24 18:28:17 -0600

    Add nav screens

--- src/index.js
+++ src/index.js
@@ -6,11 +6,32 @@ class App extends Component {
 
 render() {
   return(
-    <h1>Hello</h1>
+    <div className="app">
+      <div className="app-header">
+      </div>
+      <div className="app-wrapper">
+        <div className="app-nav">
+          <div className="nav-item screen1">
+            <p>Screen 1</p>
+            </div>
+          <div className="nav-item screen2">
+            <p>Screen 1</p>
+            </div>
+          <div className="nav-item screen3">
+            <p>Screen 1</p>
+          </div>
+        </div>
+        <div className="main-content">
+          {this.props.children}
+        </div>
+      </div>
+    </div>
   )
 }
 }
 
 ReactDOM.render(
-  <App />,
+  <App>
+    <h1>Message</h1>
+</App>,
   document.getElementById('root'));
